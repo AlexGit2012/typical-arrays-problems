43799eda589e81c778c3d1d489ba7c6b1f816cca
AlexGit2012 <a.karavaychik@gmail.com>
2020-10-03 00:48:46 +0300

    feat: add functions min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
+    if ((array!==undefined && array.length!==0) && (array.every(el=>typeof el==="number"))) {
+        let newArr = array.sort((a,b)=>a-b)
+        return newArr[0]
+    }
   return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ((array!==undefined && array.length!==0) && (array.every(el=>typeof el==="number"))) {
+        let newArr = array.sort((a,b)=>b-a)
+        return newArr[0]
+    }
+    return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ((array!==undefined && array.length!==0) && (array.every(el=>typeof el==="number"))) {
+        let value = array.reduce((acc,el)=>acc+el)
+        return value/array.length
+    }
+    return 0;
 }
